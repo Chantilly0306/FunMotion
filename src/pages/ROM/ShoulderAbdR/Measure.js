eaa7fb79ba2dc3c1079645b501d8c630f1b2817c
“Hsiang <miffy7215@gmail.com>
2025-07-17 17:03:53 +0100

    adjust fetch

--- src/pages/ROM/ShoulderAbdR/Measure.js
+++ src/pages/ROM/ShoulderAbdR/Measure.js
@@ -42,7 +42,7 @@ const Measure = () => {
     if (showResult || isFinalized) return;
 
     try {
-      const response = await fetch(process.env.REACT_APP_API_URL, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/predict`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
